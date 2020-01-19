7981e53bd4644ced432a1395d0ec11fd2dd3ef71
David Rubio <davidrubiobenito@gmail.com>
2020-01-19 20:56:15 +0100

    Cambios menu, slaider, taller, valores

--- js/db-app.js
+++ js/db-app.js
@@ -13,6 +13,7 @@ window.onscroll = function () {
 
 function myFunction() {
     var navbar = document.getElementById("myNavbar");
+    w3_close();
     if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
         navbar.className = "w3-bar" + " w3-card" + " w3-animate-top" + " w3-white";
     } else {
@@ -20,12 +21,91 @@ function myFunction() {
     }
 }
 
-// Used to toggle the menu on small screens when clicking on the menu button
-function toggleFunction() {
-    var x = document.getElementById("navDemo");
-    if (x.className.indexOf("w3-show") == -1) {
-        x.className += " w3-show";
+// Toggle between showing and hiding the sidebar when clicking the menu icon
+var mySidebar = document.getElementById("mySidebar");
+function w3_open() {
+    if (mySidebar.style.display === 'block') {
+        mySidebar.style.display = 'none';
     } else {
-        x.className = x.className.replace(" w3-show", "");
+        mySidebar.style.display = 'block';
     }
 }
+
+// Close the sidebar with the close button
+function w3_close() {
+    mySidebar.style.display = "none";
+}
+
+function pm_map(){
+    var contentPopup = '<div><div class="w3-center"><img src="../images/logo.png" alt="Prueba"></img></div><div><p><b>Perla Mayorga Boxing Club</b></p><p>28813, Pozuelo del Rey</p><p>40.367091, -3.316149</p></div></div>'
+		
+    var osmUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+        osmAttrib = '&copy; <a href="http://openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        osm = L.tileLayer(osmUrl, {maxZoom: 18, attribution: osmAttrib});
+    var map = L.map('mapid').setView([40.367091, -3.316149], 17).addLayer(osm);
+    L.marker([40.367091, -3.316149])
+        .addTo(map)
+        .bindPopup(contentPopup)
+        .openPopup();
+}
+
+function ta_setHeight(column) {
+    var maxHeight = 0;
+    //Get all the element with class = pm-cvmaestros
+    column = $(column);
+    column.css('height', 'auto');
+    //Loop all the column
+    column.each(function() {       
+        //Store the highest value
+        if($(this).height() > maxHeight) {
+            maxHeight = $(this).height();
+        }
+    });
+    //Set the height
+    column.height(maxHeight);
+}
+
+function ta_setHeightAuto(column) {
+    column = $(column);
+    column.css('height', 'auto');
+}
+
+
+
+$(document).ready(function() {
+    // executes when HTML-Document is loaded and DOM is ready
+});
+   
+$(window).on('load', function() {
+    // executes when complete page is fully loaded, including all frames, objects and images
+    //this.pm_map();
+    if($(window).width() > 580){
+        ta_setHeight('.ta-titulo-ventajas');
+        ta_setHeight('.ta-ventajas');
+    }
+    else{
+        ta_setHeight('.ta-titulo-ventajas');
+        ta_setHeightAuto('.ta-ventajas');
+    }
+
+    setTimeout(function () {
+        $("#popupCook").fadeIn(200);
+     }, 4000);
+    $("#closePopupCook, .popupCookOK").click(function() {
+        $("#popupCook").fadeOut(200);
+    }); 
+    
+});
+
+$(window).resize(function() {
+    // execute when resize window
+    if($(window).width() > 580) {
+        ta_setHeight('.ta-titulo-ventajas');
+        ta_setHeight('.ta-ventajas');
+    }
+    else{
+        ta_setHeight('.ta-titulo-ventajas');
+        ta_setHeightAuto('.ta-ventajas');
+    }
+    
+});
